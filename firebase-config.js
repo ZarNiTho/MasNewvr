ee2002965a980fe6e45b41d5713aba99bb828cb1
ZarNiTho <56052640+ZarNiTho@users.noreply.github.com>
2026-06-05 22:48:32 +0630

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
+// firebase-config.js
+const firebaseConfig = { 
+    apiKey: "AIzaSyC5sGKGeD_zht-Qknp6VPwfPIChqwvX8MQ", 
+    authDomain: "masstore-c32a7.firebaseapp.com", 
+    projectId: "masstore-c32a7", 
+    storageBucket: "masstore-c32a7.firebasestorage.app", 
+    messagingSenderId: "321720130071", 
+    appId: "1:321720130071:web:20aab19001cc8bb3cd9a52" 
+};
+
+if (!firebase.apps.length) { 
+    firebase.initializeApp(firebaseConfig); 
+}
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
